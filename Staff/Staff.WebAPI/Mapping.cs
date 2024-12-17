5d49bf4277a4641060ce92093a7b5696cf748f71
the80hz <dnl.vlgv@mail.ru>
2024-12-18 00:32:31 +0400

    Добавлен профиль отображения для маппинга сущностей и DTO с использованием AutoMapper

---
+++ Staff/Staff.WebAPI/Mapping.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using Staff.Domain.Models;
+using Staff.WebAPI.Dto;
+
+namespace Staff.WebAPI.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Employee, EmployeeDto>().ReverseMap();
+        CreateMap<Department, DepartmentDto>().ReverseMap();
+        CreateMap<Workshop, WorkshopDto>().ReverseMap();
+        CreateMap<Position, PositionDto>().ReverseMap();
+        CreateMap<Address, AddressDto>().ReverseMap();
+        CreateMap<EmploymentArchiveRecord, EmploymentArchiveRecordDto>().ReverseMap();
+        CreateMap<UnionBenefit, UnionBenefitDto>().ReverseMap();
+    }
+}
--- Staff/Staff.WebAPI/Program.cs
+++ Staff/Staff.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using AutoMapper;
+using Staff.WebAPI.Mapping;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Добавляем AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
 
