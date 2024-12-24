ca3a4e2f6446973d0e590baab2df7859f4e0df36
the80hz <dnl.vlgv@mail.ru>
2024-12-24 19:09:22 +0400

    Добавление политики CORS для разрешения всех источников, методов и заголовков

--- Staff/Staff.WebAPI/Program.cs
+++ Staff/Staff.WebAPI/Program.cs
@@ -24,6 +24,17 @@ builder.Services.AddSwaggerGen();
 // Add AutoMapper
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -38,6 +49,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAll");
 
 app.MapControllers();
 
